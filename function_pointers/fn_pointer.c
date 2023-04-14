98ae0a5b3c8e2ad60ceb1e126e48099bf5451ca3
Jerahmie Radder <jerahmie@gmail.com>
2023-04-13 19:37:55 -0500

    added function pointer test.

---
+++ function_pointers/fn_pointer.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+void myfun(int val) {
+  printf("-> inside myfun(): %d\n", val);
+}
+
+void function_caller(void (*fun)(int), int val) {
+  printf("---");
+  fun(val);
+  printf("\n");
+}
+
+int main(void) {
+  void (*fnptr) () = &myfun;
+  printf("-> inside main()\n");
+  myfun(5);
+  (*fnptr)(10);
+  function_caller(fnptr, 12);
+
+  return 0;
+}
